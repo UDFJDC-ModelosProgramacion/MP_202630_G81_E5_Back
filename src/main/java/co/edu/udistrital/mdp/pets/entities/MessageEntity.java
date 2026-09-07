6f380ebb4c716de5f356627b32bbb79db9d7c518
Ángela Sofía Puentes Medina <aspuentesm@udistrital.edu.co>
2026-09-06 21:04:12 -0500

    Add associations: Shelter-Pet, Shelter-ShelterEvent, Adopter-Message

--- src/main/java/co/edu/udistrital/mdp/pets/entities/MessageEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/MessageEntity.java
@@ -3,10 +3,13 @@ package co.edu.udistrital.mdp.pets.entities;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -17,5 +20,10 @@ public class MessageEntity extends BaseEntity {
 
     @Temporal(TemporalType.DATE)
     private Date sentDate;
+
+    @PodamExclude
+    @ManyToOne
+    @JoinColumn(name = "adopter_id")
+    private AdopterEntity adopter;
 
 }
--- src/main/java/co/edu/udistrital/mdp/pets/entities/ShelterEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/ShelterEntity.java
@@ -1,8 +1,14 @@
 package co.edu.udistrital.mdp.pets.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -11,5 +17,13 @@ public class ShelterEntity extends BaseEntity {
 
     private String name;
     private String city;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "shelter", cascade = CascadeType.PERSIST)
+    private List<PetEntity> pets = new ArrayList<>();
+
+    @PodamExclude
+    @OneToMany(mappedBy = "shelter", cascade = CascadeType.PERSIST)
+    private List<ShelterEventEntity> shelterEvents = new ArrayList<>();
 
 }
