186f0e77b07f56bf4031ad98d561a1826aac8af5
Sneyder <juansneydermendez@gmail.com>
2026-09-06 20:49:08 -0500

    Actualiza VeterinarianEntity para heredar de PersonEntity

--- src/main/java/co/edu/udistrital/mdp/pets/entities/VeterinarianEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/VeterinarianEntity.java
@@ -1,30 +1,27 @@
 package co.edu.udistrital.mdp.pets.entities;
- 
+
 import java.util.ArrayList;
 import java.util.List;
- 
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import uk.co.jemos.podam.common.PodamExclude;
- 
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Entity
-public class VeterinarianEntity extends BaseEntity {
- 
-    private String name;
-    private String email;
-    private String phone;
+public class VeterinarianEntity extends PersonEntity {
+
     private String specialty;
     private String availability;
- 
+
     @PodamExclude
     @OneToMany(mappedBy = "veterinarian", cascade = CascadeType.PERSIST)
     private List<LifeEventEntity> registeredEvents = new ArrayList<>();
- 
+
     @PodamExclude
     @OneToMany(mappedBy = "responsibleVeterinarian", cascade = CascadeType.PERSIST)
     private List<AdoptionEntity> adoptions = new ArrayList<>();
