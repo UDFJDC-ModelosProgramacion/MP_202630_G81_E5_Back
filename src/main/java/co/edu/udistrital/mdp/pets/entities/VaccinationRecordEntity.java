284c51a18dad0bc9dcb8cac304dc200fe77597eb
Sneyder <juansneydermendez@gmail.com>
2026-09-06 20:21:14 -0500

    Implementa asociaciones de Veterinarian, VaccinationRecord y LifeEvent

--- src/main/java/co/edu/udistrital/mdp/pets/entities/VaccinationRecordEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/VaccinationRecordEntity.java
@@ -3,8 +3,10 @@ package co.edu.udistrital.mdp.pets.entities;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -13,4 +15,8 @@ public class VaccinationRecordEntity extends BaseEntity {
 
     private String vaccinesApplied;
     private Date nextDueDate;
+
+    @PodamExclude
+    @OneToOne(mappedBy = "vaccinationRecord")
+    private PetEntity pet;
 }
